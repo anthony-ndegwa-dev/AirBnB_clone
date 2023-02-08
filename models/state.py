f52c7a01864463a289816b7a9998502ad0982baa
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-02-08 07:52:17 -0800

    Class that inherits from BaseModel to define a state

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Class that inherits from BaseModel to define a state
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """class to create a state"""
+    name = ""
