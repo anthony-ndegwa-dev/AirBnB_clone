d056acbc7762008a047027dca985ff2d595e1e7e
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-02-12 05:55:58 -0800

    Fix requirement failure

--- models/base_model.py
+++ models/base_model.py
@@ -1,54 +1,44 @@
-#!/usr/bin/pyhon3
-"""Module to take care of initialization, serialization, deserialization"""
-import uuid
+#!/usr/bin/python3
+"""
+This is the base model that contains serial/deserial information
+"""
 from datetime import datetime
+import uuid
 from models import storage
 
 
-class BaseModel:
-    """Defines all common attributes/methods"""
+class BaseModel():
+    """ Defines all common attributes/methods for other classes """
     def __init__(self, *args, **kwargs):
-        """initializes all attributes"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
+        """ Initializes the instances attributes """
+        if kwargs:
+            date_format = "%Y-%m-%dT%H:%M:%S.%f"
+            k_dict = kwargs.copy()
+            del k_dict["__class__"]
+            for key in k_dict:
+                if (key == "created_at" or key == "updated_at"):
+                    k_dict[key] = datetime.strptime(k_dict[key], date_format)
+            self.__dict__ = k_dict
         else:
-            f = "%Y-%m-%dT%H:%M:%S.%f"
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(kwargs[key], f)
-                if key != '__class__':
-                    setattr(self, key, value)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
+            storage.new(self)
 
     def __str__(self):
-        """returns class name, id and attribute dictionary
-        """
-        class_name = "[" + self.__class__.__name__ + "]"
-        dct = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
-        return class_name + " (" + self.id + ") " + str(dct)
+        """ Prints object in friendly format"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        """updates last modify time
-        """
-        self.updated_at = datetime.now()
+        """ Updates update_at """
+        self.updated_at = datetime.today()
         storage.save()
 
     def to_dict(self):
-        """creates a dictionary, adding a key and returning
-        datemtimes converted to strings
-        """
-        new_dict = {}
-
-        for key, values in self.__dict__.items():
-            if key == "created_at" or key == "updated_at":
-                new_dict[key] = values.strftime("%Y-%m-%dT%H:%M:%S.%f")
-            else:
-                if not values:
-                    pass
-                else:
-                    new_dict[key] = values
-        new_dict['__class__'] = self.__class__.__name__
-
+        """ Generate a new dict with an extra field __class__ """
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
